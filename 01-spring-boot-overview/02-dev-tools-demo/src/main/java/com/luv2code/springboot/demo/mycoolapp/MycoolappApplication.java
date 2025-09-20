1f8e1315d6c7560a6775ad8ace3102d4ca7b69af
Bruno Ferreira <srbrunoferreira@outlook.com>
2025-09-20 13:27:59 +0000

    support multiple maven projects

---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -0,0 +1,15 @@
+package com.luv2code.springboot.demo.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoolappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MycoolappApplication.class, args);
+
+        System.out.println("Hello World!");
+    }
+
+}
--- 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class FunRestController {
   @GetMapping("/")
   public String sayHello() {
-    return "Hello, World from Spring Boot demo!";
+    return "Hello, World from Dev Tools Demo21!";
+  }
+
+  @GetMapping("/workout")
+  public String getDailyWorkout() {
+    return "Run a hard 5k!";
   }
 }
