ef4b7753e4b137f77a082522d17e14969f2b6bc6
Bruno Ferreira <srbrunoferreira@outlook.com>
2025-09-21 11:09:50 +0000

    05-properties-demo

---
+++ 01-spring-boot-overview/05-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,31 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class FunRestController {
+  @Value("${coach.name}")
+  private String coachName;
+
+  @Value("${team.name}")
+  private String teamName;
+
+  @GetMapping("/")
+  public String sayHello() {
+    return "Hello, World from Actuator Demo!";
+  }
+
+  @GetMapping("/workout")
+  public String getDailyWorkout() {
+    return "Run a hard 5k!";
+  }
+
+  @GetMapping("/teaminfo")
+  public String getTeamInfo() {
+      return "Coach:" + coachName + ", Team name: " + teamName;
+  }
+}
