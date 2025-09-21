96d8d25673ce8ab1c47f9e1bb5e209ba8cc4bda5
Bruno Ferreira <srbrunoferreira@outlook.com>
2025-09-21 10:12:58 +0000

    bkp

---
+++ 01-spring-boot-overview/03-actuator-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,17 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+  @GetMapping("/")
+  public String sayHello() {
+    return "Hello, World from Actuator Demo!";
+  }
+
+  @GetMapping("/workout")
+  public String getDailyWorkout() {
+    return "Run a hard 5k!";
+  }
+}
